ef10863ceca567476ccb97eb54b1208c483ef194
DMI <97788493+Dmiiy@users.noreply.github.com>
2024-05-18 19:31:44 +0300

    phone auth

--- main.py
+++ main.py
@@ -1,67 +1,76 @@
-import os
-import pyrebase
+from cffi.backend_ctypes import unicode
 from flask import Flask, render_template, request
-
-config={
-    'apiKey': os.environ['FIREBASE_API_KEY'],
-    'authDomain': "wish-1a863.firebaseapp.com",
-    'projectId': "wish-1a863",
-    'storageBucket': "wish-1a863.appspot.com",
-    'messagingSenderId': "876599678293",
-    'appId': "1:876599678293:web:6b15d4438ee75739bc8bdc",
-    'measurementId': "G-NXXWC0HMCV",
-    "databaseURL": ""
-}
-
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+import hashlib
+import json
+import requests
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index',methods=['GET','POST'])
+login = 'wish'
+ts = unicode(time.time())
+api_key = 'etqiJGoBqPoNWsJtELHUBLlg'
+secret = hashlib.md5((ts + api_key).encode()).hexdigest()
+
+def generate_code():
+    """Генерация случайного кода заданной длины."""
+    import random
+    return ''.join(random.choice('0123456789') for i in range(4))
+
+headers = {
+    'login': login,
+    'ts': ts,
+    'secret': secret,
+    'Content-type': 'application/json',
+}
+data = {
+    'route': 'fcall',
+    'to': '+79993332211',
+    'text': generate_code()
+}
+
+def send_sms_code():
+    """Отправка SMS-кода для авторизации."""
+    r = requests.post('https://cp.redsms.ru/api/message', headers=headers, data=json.dumps(data))
+
+    if r.status_code == 200:
+        return True
+    else:
+        return False
+
+@app.route("/")
 def index():
-    if request.method=='POST':
-        email=request.form['user_email']
-        password= request.form['user_pas']
-        try:
-            auth.sign_in_with_email_and_password(email,password)
-            user_info=auth.sign_in_with_email_and_password(email,password)
-            account_info=auth.get_account_info(user_info['idToken'])
-            if account_info["users"][0]["emailVerified"]== False:
-                verify_message = 'Please verify your email'
-                return render_template('index.html',umessage=verify_message)
-            return render_template('home.html')
-        except:
-            unsuccessful= 'Please check your credentials'
-            return render_template('index.html',umessage=unsuccessful)
-    return render_template('index.html')
+    """Первая страница с вводом номера телефона."""
+    return render_template("index.html")
 
-@app.route('/create_account',methods =['GET','POST'])
-def create_account():
-    if request.method == 'POST':
-        pas0=request.form['user_pas0']
-        pas1=request.form['user_pas1']
-        if pas1==pas0:
-            try:
-                email=request.form['user_email']
-                password=request.form['user_pas0']
-                new_user= auth.create_user_with_email_and_password(email,password)
-                auth.send_email_verification(new_user['idToken'])
-                return render_template('verify_email.html')
-            except:
-                existing_account='This email is already used'
-                return render_template('create_account.html',exist_message=existing_account)
+@app.route("/verify", methods=["POST"])
+def verify():
+    """Обработка формы с вводом номера телефона."""
+    phone_number = request.form["phone_number"]
+    data['to'] = phone_number
+    recieve = send_sms_code()
+    if recieve:
+        return render_template("verify.html", code=recieve, phone_number=phone_number)
+    else:
+        return "Error"
 
-    return render_template('create_account.html')
+@app.route("/confirm", methods=["POST"])
+def confirm():
+    """Обработка формы с вводом кода подтверждения."""
+    phone_number = request.form["phone_number"]
+    entered_code = request.form["code"]
 
-@app.route('/reset_password',methods =['GET','POST'])
-def forgot_password():
-    if request.method=='POST':
-        email=request.form['user_email']
-        auth.send_password_reset_email(email)
-        return render_template('index.html')
-    return render_template('reset_password.html')
+    if entered_code==data['text']:
+        is_verified=True
+    else:
+        is_verified=False
+
+    if is_verified:
+        # Авторизация прошла успешно
+        return "Авторизация прошла успешно!"
+    else:
+        return "Неверный код подтверждения"
+
 
 if __name__ == "__main__":
     app.run()
